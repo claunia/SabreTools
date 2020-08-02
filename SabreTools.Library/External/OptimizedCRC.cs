ec524a63ca508a1e0fc09b62b4d0cee5c71dc3dd
Matt Nadareski <mnadareski@outlook.com>
2020-08-01 23:12:44 -0700

    External code goes in external

--- SabreTools.Library/External/OptimizedCRC.cs
+++ SabreTools.Library/External/OptimizedCRC.cs
@@ -24,7 +24,7 @@
 
 using System;
 
-namespace SabreTools.Library.Tools
+namespace SabreTools.Library.External
 {
     public class OptimizedCRC : IDisposable
     {
--- SabreTools.Library/Tools/Hasher.cs
+++ SabreTools.Library/Tools/Hasher.cs
@@ -4,6 +4,7 @@ using System.Security.Cryptography;
 using System.Threading.Tasks;
 
 using SabreTools.Library.DatFiles;
+using SabreTools.Library.External;
 
 namespace SabreTools.Library.Tools
 {
