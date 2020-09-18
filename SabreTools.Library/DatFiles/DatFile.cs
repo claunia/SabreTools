3502a35dbd39c8f1a2f6a5c140af0291e28c40ee
Matt Nadareski <mnadareski@outlook.com>
2020-09-18 10:44:01 -0700

    Remove unused parameter from Write

--- SabreTools.Library/DatFiles/DatFile.cs
+++ SabreTools.Library/DatFiles/DatFile.cs
@@ -3422,12 +3422,11 @@ namespace SabreTools.Library.DatFiles
         /// Create and open an output file for writing direct from a dictionary
         /// </summary>
         /// <param name="outDir">Set the output directory (current directory on null)</param>
-        /// <param name="norename">True if games should only be compared on game and file name (default), false if system and source are counted</param>
         /// <param name="ignoreblanks">True if blank roms should be skipped on output, false otherwise (default)</param>
         /// <param name="overwrite">True if files should be overwritten (default), false if they should be renamed instead</param>
         /// <param name="throwOnError">True if the error that is thrown should be thrown back to the caller, false otherwise</param>
         /// <returns>True if the DAT was written correctly, false otherwise</returns>
-        public bool Write(string outDir, bool norename = true, bool ignoreblanks = false, bool overwrite = true, bool throwOnError = false)
+        public bool Write(string outDir, bool ignoreblanks = false, bool overwrite = true, bool throwOnError = false)
         {
             // If we have nothing writable, abort
             if (!HasWritable())
@@ -3450,7 +3449,7 @@ namespace SabreTools.Library.DatFiles
             EnsureHeaderFields();
 
             // Bucket roms by game name, if not already
-            Items.BucketBy(Field.Machine_Name, DedupeType.None, norename: norename);
+            Items.BucketBy(Field.Machine_Name, DedupeType.None);
 
             // Output the number of items we're going to be writing
             Globals.Logger.User($"A total of {Items.TotalCount - Items.RemovedCount} items will be written out to '{Header.FileName}'");
