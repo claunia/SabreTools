2e7f70b53504370c8a357dd6627eaf821c0b7277
Matt Nadareski <mnadareski@outlook.com>
2020-12-11 23:06:40 -0800

    Version string -> Prepare

--- SabreTools.Core/Constants.cs
+++ SabreTools.Core/Constants.cs
@@ -8,13 +8,6 @@ namespace SabreTools.Core
     /// </summary>
     public static class Constants
     {
-        /// <summary>
-        /// The current toolset version to be used by all child applications
-        /// </summary>
-        //public readonly static string Version = $"v1.0.4";
-        public readonly static string Version = $"v1.0.4-{File.GetCreationTime(Assembly.GetExecutingAssembly().Location):yyyy-MM-dd HH:mm:ss}";
-        public const int HeaderHeight = 3;
-
         #region 0-byte file constants
 
         public const long SizeZero = 0;
--- SabreTools.Core/Prepare.cs
+++ SabreTools.Core/Prepare.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Reflection;
 
 namespace SabreTools.Core
 {
@@ -8,6 +10,12 @@ namespace SabreTools.Core
     public static class Prepare
     {
         /// <summary>
+        /// The current toolset version to be used by all child applications
+        /// </summary>
+        //public readonly static string Version = $"v1.0.4";
+        public readonly static string Version = $"v1.0.4-{File.GetCreationTime(Assembly.GetExecutingAssembly().Location):yyyy-MM-dd HH:mm:ss}";
+ 
+        /// <summary>
         /// Readies the console and outputs the header
         /// </summary>
         /// <param name="program">The name to be displayed as the program</param>
@@ -16,7 +24,7 @@ namespace SabreTools.Core
             // Dynamically create the header string, adapted from http://stackoverflow.com/questions/8200661/how-to-align-string-in-fixed-length-string
             int width = Console.WindowWidth - 3;
             string border = $"+{new string('-', width)}+";
-            string mid = $"{program} {Constants.Version}";
+            string mid = $"{program} {Version}";
             mid = $"|{mid.PadLeft(((width - mid.Length) / 2) + mid.Length).PadRight(width)}|";
 
             // If we're outputting to console, do fancy things
@@ -34,7 +42,7 @@ namespace SabreTools.Core
                     Console.BackgroundColor = ConsoleColor.Blue;
                 }
 
-                Console.Title = $"{program} {Constants.Version}";
+                Console.Title = $"{program} {Version}";
 
                 // Output the header
                 Console.WriteLine(border);
