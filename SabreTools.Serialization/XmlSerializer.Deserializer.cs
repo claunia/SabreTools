8cf93a4b3ba88cc74c9e510744bbf4b0b953cf83
Matt Nadareski <mnadareski@outlook.com>
2023-07-28 10:28:44 -0400

    Split first 2 serializers into partial classes

---
+++ SabreTools.Serialization/XmlSerializer.Deserializer.cs
@@ -0,0 +1,64 @@
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace SabreTools.Serialization
+{
+    /// <summary>
+    /// XML deserializer for nullable types
+    /// </summary>
+    public abstract partial class XmlSerializer<T>
+    {
+        /// <summary>
+        /// Deserializes an XML file to the defined type
+        /// </summary>
+        /// <param name="path">Path to the file to deserialize</param>
+        /// <returns>Deserialized data on success, null on failure</returns>
+        public static T? Deserialize(string path)
+        {
+            try
+            {
+                using var stream = PathProcessor.OpenStream(path);
+                return Deserialize(stream);
+            }
+            catch
+            {
+                // TODO: Handle logging the exception
+                return default;
+            }
+        }
+
+        /// <summary>
+        /// Deserializes an XML file in a stream to the defined type
+        /// </summary>
+        /// <param name="stream">Stream to deserialize</param>
+        /// <returns>Deserialized data on success, null on failure</returns>
+        public static T? Deserialize(Stream? stream)
+        {
+            try
+            {
+                // If the stream is null
+                if (stream == null)
+                    return default;
+
+                // Setup the serializer and the reader
+                var serializer = new XmlSerializer(typeof(T));
+                var settings = new XmlReaderSettings
+                {
+                    CheckCharacters = false,
+                    DtdProcessing = DtdProcessing.Ignore,
+                };
+                var streamReader = new StreamReader(stream);
+                var xmlReader = XmlReader.Create(streamReader, settings);
+
+                // Perform the deserialization and return
+                return (T?)serializer.Deserialize(xmlReader);
+            }
+            catch
+            {
+                // TODO: Handle logging the exception
+                return default;
+            }
+        }
+    }
+}
--- SabreTools.Serialization/XmlSerializer.Serializer.cs
+++ SabreTools.Serialization/XmlSerializer.Serializer.cs
@@ -7,66 +7,8 @@ namespace SabreTools.Serialization
     /// <summary>
     /// XML serializer for nullable types
     /// </summary>
-    public abstract class XmlSerializer<T>
+    public abstract partial class XmlSerializer<T>
     {
-        #region Deserialization
-
-        /// <summary>
-        /// Deserializes an XML file to the defined type
-        /// </summary>
-        /// <param name="path">Path to the file to deserialize</param>
-        /// <returns>Deserialized data on success, null on failure</returns>
-        public static T? Deserialize(string path)
-        {
-            try
-            {
-                using var stream = PathProcessor.OpenStream(path);
-                return Deserialize(stream);
-            }
-            catch
-            {
-                // TODO: Handle logging the exception
-                return default;
-            }
-        }
-
-        /// <summary>
-        /// Deserializes an XML file in a stream to the defined type
-        /// </summary>
-        /// <param name="stream">Stream to deserialize</param>
-        /// <returns>Deserialized data on success, null on failure</returns>
-        public static T? Deserialize(Stream? stream)
-        {
-            try
-            {
-                // If the stream is null
-                if (stream == null)
-                    return default;
-
-                // Setup the serializer and the reader
-                var serializer = new XmlSerializer(typeof(T));
-                var settings = new XmlReaderSettings
-                {
-                    CheckCharacters = false,
-                    DtdProcessing = DtdProcessing.Ignore,
-                };
-                var streamReader = new StreamReader(stream);
-                var xmlReader = XmlReader.Create(streamReader, settings);
-
-                // Perform the deserialization and return
-                return (T?)serializer.Deserialize(xmlReader);
-            }
-            catch
-            {
-                // TODO: Handle logging the exception
-                return default;
-            }
-        }
-
-        #endregion
-
-        #region Serialization
-
         /// <summary>
         /// Serializes the defined type to an XML file
         /// </summary>
@@ -125,7 +67,5 @@ namespace SabreTools.Serialization
                 return null;
             }
         }
-    
-        #endregion
     }
 }
