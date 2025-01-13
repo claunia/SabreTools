47b618be163469125e26188ce28bbd7f03e2d0ea
Matt Nadareski <mnadareski@outlook.com>
2025-01-13 00:15:36 -0500

    Add skeleton test file for Splitting

---
+++ SabreTools.DatFiles.Test/DatFileTests.Splitting.cs
@@ -0,0 +1,55 @@
+using Xunit;
+
+namespace SabreTools.DatFiles.Test
+{
+    public partial class DatFileTests
+    {
+        #region AddRomsFromBios
+
+        // TODO: Implement AddRomsFromBios tests
+
+        #endregion
+
+        #region AddRomsFromChildren
+
+        // TODO: Implement AddRomsFromChildren tests
+
+        #endregion
+
+        #region AddRomsFromDevices
+
+        // TODO: Implement AddRomsFromDevices tests
+
+        #endregion
+
+        #region AddRomsFromParent
+
+        // TODO: Implement AddRomsFromParent tests
+
+        #endregion
+
+        #region RemoveBiosAndDeviceSets
+
+        // TODO: Implement RemoveBiosAndDeviceSets tests
+
+        #endregion
+
+        #region RemoveBiosRomsFromChild
+
+        // TODO: Implement RemoveBiosRomsFromChild tests
+
+        #endregion
+
+        #region RemoveRomsFromChild
+
+        // TODO: Implement RemoveRomsFromChild tests
+
+        #endregion
+
+        #region RemoveTagsFromChild
+
+        // TODO: Implement RemoveTagsFromChild tests
+
+        #endregion
+    }
+}
