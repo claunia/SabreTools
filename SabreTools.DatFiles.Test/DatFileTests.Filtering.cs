1469edb302e60eb1167c71ea5a4ea9ffbbfe22da
Matt Nadareski <mnadareski@outlook.com>
2025-01-13 21:14:00 -0500

    Add filtering test skeleton

---
+++ SabreTools.DatFiles.Test/DatFileTests.Filtering.cs
@@ -0,0 +1,34 @@
+using SabreTools.DatFiles.Formats;
+using SabreTools.DatItems;
+using SabreTools.DatItems.Formats;
+using Xunit;
+
+namespace SabreTools.DatFiles.Test
+{
+    public partial class DatFileTests
+    {
+        #region MachineDescriptionToName
+
+        // TODO: Implement MachineDescriptionToName tests
+
+        #endregion
+
+        #region SetOneRomPerGame
+
+        // TODO: Implement SetOneRomPerGame tests
+
+        #endregion
+
+        #region SetOneGamePerRegion
+
+        // TODO: Implement SetOneGamePerRegion tests
+
+        #endregion
+
+        #region StripSceneDatesFromItems
+
+        // TODO: Implement StripSceneDatesFromItems tests
+
+        #endregion
+    }
+}
