687c5fe58c2fb63776ac261a0080915b64bc3489
Matt Nadareski <mnadareski@outlook.com>
2025-02-10 12:39:57 -0500

    Add DatStatistics test scaffolding

---
+++ SabreTools.DatFiles.Test/DatStatisticsTests.cs
@@ -0,0 +1,61 @@
+using System.Collections.Generic;
+using System.IO;
+using SabreTools.DatFiles.Formats;
+using SabreTools.DatItems;
+using SabreTools.DatItems.Formats;
+using SabreTools.Hashing;
+using Xunit;
+
+namespace SabreTools.DatFiles.Test
+{
+    public class DatStatisticsTests
+    {
+        #region Constructor
+
+        // TODO: Write Constructor tests
+
+        #endregion
+
+        #region AddItemStatistics
+
+        // TODO: Write AddItemStatistics tests
+
+        #endregion
+
+        #region AddStatistics
+
+        // TODO: Write AddStatistics tests
+
+        #endregion
+
+        #region GetHashCount
+
+        // TODO: Write GetHashCount tests
+
+        #endregion
+
+        #region GetItemCount
+
+        // TODO: Write GetItemCount tests
+
+        #endregion
+
+        #region GetStatusCount
+
+        // TODO: Write GetStatusCount tests
+
+        #endregion
+
+        #region RemoveItemStatistics
+
+        // TODO: Write RemoveItemStatistics tests
+
+        #endregion
+
+        #region ResetStatistics
+
+        // TODO: Write ResetStatistics tests
+
+        #endregion
+    }
+}
