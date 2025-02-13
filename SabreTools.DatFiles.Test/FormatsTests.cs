94ffd0fa0d1f615838d89b4b131938653ef2403f
Matt Nadareski <mnadareski@outlook.com>
2025-02-13 15:01:03 -0500

    Add DatFile format tests skeleton

---
+++ SabreTools.DatFiles.Test/FormatsTests.cs
@@ -0,0 +1,118 @@
+using System.Collections.Generic;
+using System.IO;
+using SabreTools.DatFiles.Formats;
+using SabreTools.DatItems;
+using SabreTools.DatItems.Formats;
+using SabreTools.Hashing;
+using Xunit;
+
+namespace SabreTools.DatFiles.Test
+{
+    /// <summary>
+    /// Contains tests for all specific DatFile formats
+    /// </summary>
+    public class FormatsTests
+    {
+        #region ArchiveDotOrg
+
+        // TODO: Write ArchiveDotOrg format tests
+
+        #endregion
+
+        #region AttractMode
+
+        // TODO: Write AttractMode format tests
+
+        #endregion
+
+        #region ClrMamePro
+
+        // TODO: Write ClrMamePro format tests
+
+        #endregion
+
+        #region DosCenter
+
+        // TODO: Write DosCenter format tests
+
+        #endregion
+
+        #region EverdriveSmdb
+
+        // TODO: Write EverdriveSmdb format tests
+
+        #endregion
+
+        #region Hashfile
+
+        // TODO: Write Hashfile format tests
+
+        #endregion
+
+        #region Listrom
+
+        // TODO: Write Listrom format tests
+
+        #endregion
+
+        #region Listxml
+
+        // TODO: Write Listxml format tests
+
+        #endregion
+
+        #region Logiqx
+
+        // TODO: Write Logiqx format tests
+
+        #endregion
+
+        #region Missfile
+
+        // TODO: Write Missfile format tests
+
+        #endregion
+
+        #region OfflineList
+
+        // TODO: Write OfflineList format tests
+
+        #endregion
+
+        #region OpenMSX
+
+        // TODO: Write OpenMSX format tests
+
+        #endregion
+
+        #region RomCenter
+
+        // TODO: Write RomCenter format tests
+
+        #endregion
+
+        #region SabreJSON
+
+        // TODO: Write SabreJSON format tests
+
+        #endregion
+
+        #region SabreXML
+
+        // TODO: Write SabreXML format tests
+
+        #endregion
+
+        #region SeparatedValue
+
+        // TODO: Write SeparatedValue format tests
+
+        #endregion
+
+        #region SoftwareList
+
+        // TODO: Write SoftwareList format tests
+
+        #endregion
+    }
+}
