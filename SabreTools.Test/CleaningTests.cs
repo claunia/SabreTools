0748cd7f4da605f8ca9eff5215626f59aff36f84
Matt Nadareski <mnadareski@outlook.com>
2025-02-19 14:06:14 -0500

    Remove unnecessary Cleaning test helpers

--- SabreTools.Test/CleaningTests.cs
+++ SabreTools.Test/CleaningTests.cs
@@ -10,8 +10,12 @@ namespace SabreTools.Test
         [Fact]
         public void CleanDatItem_Normalize()
         {
-            var datItem = CreateDatItem("name");
-            var machine = CreateMachine("\"ÁБ\"", "ä|/Ж");
+            var datItem = new Rom();
+            datItem.SetName("name");
+
+            var machine = new Machine();
+            machine.SetFieldValue<string?>(Models.Metadata.Machine.NameKey, "\"ÁБ\"");
+            machine.SetFieldValue<string?>(Models.Metadata.Machine.DescriptionKey, "ä|/Ж");
 
             var cleaner = new Cleaner { Normalize = true };
             cleaner.CleanDatItem(datItem, machine);
@@ -24,9 +28,12 @@ namespace SabreTools.Test
         [Fact]
         public void CleanDatItem_RemoveUnicode()
         {
-            var datItem = CreateDatItem("nam诶");
-            var machine = CreateMachine("nam诶-2", "nam诶-3");
+            var datItem = new Rom();
+            datItem.SetName("nam诶");
 
+            var machine = new Machine();
+            machine.SetFieldValue<string?>(Models.Metadata.Machine.NameKey, "nam诶-2");
+            machine.SetFieldValue<string?>(Models.Metadata.Machine.DescriptionKey, "nam诶-3");
 
             var cleaner = new Cleaner { RemoveUnicode = true };
             cleaner.CleanDatItem(datItem, machine);
@@ -39,8 +46,12 @@ namespace SabreTools.Test
         [Fact]
         public void CleanDatItem_Single()
         {
-            var datItem = CreateDatItem("name");
-            var machine = CreateMachine("name-2", "name-3");
+            var datItem = new Rom();
+            datItem.SetName("name");
+
+            var machine = new Machine();
+            machine.SetFieldValue<string?>(Models.Metadata.Machine.NameKey, "name-2");
+            machine.SetFieldValue<string?>(Models.Metadata.Machine.DescriptionKey, "name-3");
 
             var cleaner = new Cleaner { Single = true };
             cleaner.CleanDatItem(datItem, machine);
@@ -57,8 +68,12 @@ namespace SabreTools.Test
         [InlineData("C:\\AbnormalFolderLengthPath\\ThatReallyPushesTheLimit\\OfHowLongYou\\ReallyShouldNameThings\\AndItGetsEvenWorse\\TheMoreSubfoldersThatYouTraverse\\BecauseWhyWouldYouStop\\AtSomethingReasonable\\LikeReallyThisIsGettingDumb\\AndIKnowItsJustATest\\ButNotAsMuchAsMe", "nam")]
         public void CleanDatItem_TrimRoot(string? root, string expected)
         {
-            var datItem = CreateDatItem("name");
-            var machine = CreateMachine("name-2", "name-3");
+            var datItem = new Rom();
+            datItem.SetName("name");
+
+            var machine = new Machine();
+            machine.SetFieldValue<string?>(Models.Metadata.Machine.NameKey, "name-2");
+            machine.SetFieldValue<string?>(Models.Metadata.Machine.DescriptionKey, "name-3");
 
             var cleaner = new Cleaner
             {
@@ -70,31 +85,6 @@ namespace SabreTools.Test
             Assert.Equal(expected, datItem.GetName());
             Assert.Equal("name-2", datItem.GetFieldValue<Machine>(DatItem.MachineKey)!.GetStringFieldValue(Models.Metadata.Machine.NameKey));
             Assert.Equal("name-3", datItem.GetFieldValue<Machine>(DatItem.MachineKey)!.GetStringFieldValue(Models.Metadata.Machine.DescriptionKey));
-        }
-
-        /// <summary>
-        /// Generate a consistent DatItem for testing
-        /// </summary>
-        private static DatItem CreateDatItem(string name)
-        {
-            var rom = new Rom();
-
-            rom.SetName(name);
-
-            return rom;
-        }
-
-        /// <summary>
-        /// Generate a consistent Machine for testing
-        /// </summary>
-        private static Machine CreateMachine(string machName, string desc)
-        {
-            var machine = new Machine();
-
-            machine.SetFieldValue<string?>(Models.Metadata.Machine.NameKey, machName);
-            machine.SetFieldValue<string?>(Models.Metadata.Machine.DescriptionKey, desc);
-
-            return machine;
         }
     }
 }
