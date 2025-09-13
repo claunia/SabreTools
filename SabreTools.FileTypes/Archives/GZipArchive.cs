2e93fe340d2d77aac9027269a3c3ebdc3a56794e
Natalia Portillo <claunia@claunia.com>
2025-09-13 12:45:25 +0100

    Change syntax for .NET 10 compatibility.

--- SabreTools.FileTypes/Archives/GZipArchive.cs
+++ SabreTools.FileTypes/Archives/GZipArchive.cs
@@ -476,7 +476,7 @@ namespace SabreTools.FileTypes.Archives
                 ds.Dispose();
 
                 // Now write the standard footer
-                sw.Write([.. baseFile.CRC!.Reverse()]);
+                sw.Write([.. System.Linq.Enumerable.Reverse(baseFile.CRC!)]);
                 sw.Write((uint)(baseFile.Size ?? 0));
 
                 // Dispose of everything
